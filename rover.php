3841dd8465822c5409242e2cccff4c13d090a9e1
VitorHugoS <vitorhugoss17@gmail.com>
2016-09-02 13:39:34 -0300

    Update rover.php

--- rover.php
+++ rover.php
@@ -1,25 +1,54 @@
 <?php
 
+//Criacao da classe rover, para criação de nosso objeto
+/*
+Entendendo o basico do exercicio
+O intuito do programa é movimentar um objeto em um plano cartesiano, movimentando entre x e y de acordo para onde o objeto esta voltado
+Imagine os 4 pontos cardeais, Norte, Sul, Leste Oeste, a partir de agora cada um vai receber um grau
+	Norte = 0
+	Sul = 180
+	Leste = 90
+	Oeste = 270
+Tendo estes valores em mente, iremos inciar o rover, ele aceita 3 tipos de instruçao, L,R,M
+	L = Left -> Esquerda (-90 graus)
+	R = Right -> Direta (+90 graus)
+	M = Move -> Movimentar (Movimentar)
+L, R, são reponsaveis por girar o rover e M, para movimentar, porém a peculiaridade em questão são os métodos, Direita e Esquerda
+Ou seja, Direita adiciona mais 90 graus, e esquerda retira 90 graus, visto isso se temos um objeto ao sul, e viramos a esquerda, ele vai para o leste, pois:
+Sul  = 180 - 90 = 90, o valor de leste é 90
+*/
 class Rover{
-	//posicoes do plano cartesiano X, Y
-	private $x=0;
-	private $y=0;
-	//por padrao o rover sempre iniciara com a face para o norte
-	private $Orientacao="N";
-
-	//iniciar a posicao do rover, valores padroes caso a funcao seja chamada vazia
-    function posicao($posicaox=0, $posicaoy=0, $face="N"){
-		//setar as posicoes do plano cartesiano
-		$this->x=$posicaox;
-		$this->y=$posicaoy;
-		//setar face do rover
-		$this->Orientacao=$face;
+	//atributos de nossa classe
+	//x e y, são posicoes do plano cartesiano
+	private $x;
+	private $y;
+	//face e para onde o rover vai estar voltado, norte, sul, leste ou oeste
+	private $face;
+	//posicoes, serve apenas para validarmos, se o usuario colocou, uma posicao valida, norte, sul, leste ou oeste
+	//setamos as posicoes validas
+	private $posicoes = array("N", "L", "S", "O");
+	//metodos do rover
+	//construtor do objeto, seta os parametros automaticamente
+	//os 3 atributos tem valores padrão, ou seja, caso quando instanciarmos, não passarmos as posições, o padrão será x-> 0, y->0, face->norte
+	public function __construct($x = 0, $y =0, $face = "N"){
+		//o this referencia a um proprio atributo da classe, entao checamos se o numero é um inteiro, se sim, atribuimos a x e y, se não, valor padrao de 0
+		if(is_int($x)){
+			$this->x = $x;
+		}
+		if(is_int($x)){
+			$this->y = $y;
+		}
+		//aqui checamos se a face esta setada para um posição valida, ou seja, se for, norte, sul, leste, ou oeste, ele seta, se não, seta norte, por padrao
+		if(in_array($face, $this->posicoes)){
+			$this->face = $face;
+		}else{
+			$this->face = "N";
+		}
 	}
-	//movimenta o hover
+	//metodo move, movimentar o rover para onde sua face esta voltada, ou seja, se ele esta em 0,0 e para norte, ele andara no eixo X para cima, se ele esta em 0,0 para sul, andara no eixo X para baixo
 	function movimentar($movimentos){
 		//percorre string e executa os movimentos
-		for($i=0; $i<strlen($movimentos);$i++)
-		{
+		for($i=0; $i<strlen($movimentos);$i++){
 			switch ($movimentos[$i]) {
 				case 'L':
 					//funcao para esquerda
@@ -36,49 +65,55 @@ class Rover{
 				default:
 					echo "Comando inexistente!";
 				break;
+			//fimswitch
 			}
+		//fimfor
 		}
 	}
 	//funcao esquerda
 	function esquerda(){
 		//gira o rover
-		switch($this->Orientacao){
+		switch($this->face){
+			//caso for norte, vire para oeste
 			case "N":
-				$this->Orientacao="W";
+				$this->face="O";
 			break;
+			//caso for sul, vire para o leste
 			case "S":
-				$this->Orientacao="E";
+				$this->face="L";
 			break;
-			case "E":
-				$this->Orientacao="N";
+			//caso for oeste, vire para o sul
+			case "O":
+				$this->face="S";
 			break;
-			case "W":
-				$this->Orientacao="S";
+			//caso for leste, vire para o norte
+			case "L":
+				$this->face="N";
 			break;
 		}
 	}
 	//funcao direita
 	function direita(){
 		//gira o rover
-		switch($this->Orientacao){
+		switch($this->face){
 			case "N":
-				$this->Orientacao="E";
+				$this->face="L";
 			break;
 			case "S":
-				$this->Orientacao="W";
+				$this->face="O";
 			break;
 			case "E":
-				$this->Orientacao="S";
+				$this->face="S";
 			break;
-			case "W":
-				$this->Orientacao="N";
+			case "O":
+				$this->face="N";
 			break;
 		}
 	}
 	//funcao mover
 	function mover(){
 		//move o rover uma casa a frente para onde sua face esta voltada
-		switch($this->Orientacao){
+		switch($this->face){
 			case "N":
 				$this->y++;
 			break;
@@ -86,27 +121,29 @@ class Rover{
 				$this->y--;
 			break;
 			case "E":
-				$this->x++;
+				$this->n++;
 			break;
 			case "W":
-				$this->x--;
+				$this->n--;
 			break;
 		}
 	}
-
 	//mostra a posicao atual do rover
 	function imprimir(){
-		echo "X: (".$this->x.") - Y: (".$this->y.") Face - >".$this->Orientacao;
+		echo "X: (".$this->x.") - Y: (".$this->y.") Face - >".$this->face,"<br>";
 	}
 
-	
 }
+//instancia, cria, o nosso objeto rover
+$rover = new Rover();
+//Executa os comandos de movimentação
+$rover->movimentar("M");
+//imprime sua posição
+$rover->imprimir();
 
-$rover=new Rover();
-$rover->posicao(1, 2, "N");
-$rover->movimentar("LMLMLMLMM");
+//instancia, cria, o nosso objeto rover
+$rover = new Rover(5,2,"S");
+//Executa os comandos de movimentação
+$rover->movimentar("MLLRRMMMMR");
+//imprime sua posição
 $rover->imprimir();
-$rover->posicao(3, 3, "E");
-$rover->movimentar("MMRMMRMRRM");
-$rover->imprimir();
-?>
